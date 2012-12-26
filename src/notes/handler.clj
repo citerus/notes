f88fe934276b21e1f8516be15d112fbd7689426f
patrikfr Patrik Fredriksson <patrik@snart.nu>
2012-12-26 04:12:41 +0100

    Added styling by Bootstrap.

--- src/notes/handler.clj
+++ src/notes/handler.clj
@@ -3,6 +3,7 @@
         [hiccup.core]
         [hiccup.form]
         [hiccup.page]
+        [hiccup.element]
         [ring.util.response])
   
   (:require [compojure.handler :as handler]
@@ -32,10 +33,29 @@
   (html 
    (html5
       [:head [:title "Notes"]
-       (include-css "/notes.css")]
+       (include-css "/css/bootstrap.css")
+       (include-css "/css/notes.css")
+       (include-js "/js/bootstrap.js")
+       [:style
+        "body {
+          padding-top: 60px;
+        }"]]
+    
       [:body
-			[:h1 "Notes!"]
-      (if added? (html "Note added!" [:br]))
+       [:div.navbar.navbar-fixed-top
+        [:div.navbar-inner
+         [:div.container
+         (link-to {:class "brand"} "#" "Notes!")]]]
+       
+       [:header
+        [:div.container
+          [:h1 "Notes!"]
+         [:p.lead [:small " Create and read awesome note, right here!"]]]]
+       
+       [:div.container
+        [:div.row-fluid	
+         [:div.span4
+          
         
       (form-to [:post "/"]
                  "Heading"
@@ -46,17 +66,23 @@
                  [:br]
                  (text-area :body)
                  [:br]
-                 (submit-button "submit"))
-  
-      [:table
-       [:tr [:th "Subject"] [:th "Note"] [:th "TS"] [:th]]
-       (for [note notes]
-         [:tr 
-           [:td (:heading note)] 
-           [:td (:body note)] 
-           [:td (:ts note)]
-           [:td (form-to [:delete "/"] (hidden-field :id (:_id note)) (submit-button "Delete"))]])]])))
-
+                 [:button.btn {:type "submit"} "Create Note!"] " " (if added? (html [:i.icon-ok])))]
+         
+         [:div.span8 
+          (for [note notes]
+            [:div
+            [:div.row-fluid 
+              [:div.span10 [:h2 [:small (:heading note)]]]
+              [:div.span2 (form-to [:delete "/"] (hidden-field :id (:_id note)) [:button.btn.btn-mini.btn-link {:type "submit"} [:i.icon-remove]])]]
+            [:div (:body note)]
+            [:p.text-info [:small (:ts note)]]])]]]]
+    
+    [:footer 
+     [:div.container 
+      [:p.muted.credit "By " (link-to "http://www.citerus.se/" "Citerus") " " [:i.icon-star-empty]
+       " Styling support by " (link-to "http://twitter.github.com/bootstrap/index.html" "Bootstrap. ") [:i.icon-star-empty]
+       " Icons by " (link-to "http://glyphicons.com/" "Glyphicons")]]])))
+    
 
 (defroutes app-routes
   (GET "/" [] (front-page (find-notes) false))
@@ -69,7 +95,9 @@
   (DELETE "/" [id]
          (do
           (delete-note! id)
-          (front-page (find-notes) true)))
+          (front-page (find-notes) false)))
+
+  (route/resources "/")
   
   (route/not-found "Not Found"))
 
