307e749d1ddd7ca9b47b7766530a36d6528353b8
Patrik Fredriksson <patrik.fredriksson@citerus.se>
2012-12-30 21:15:50 +0100

    Added a logo. Formatted code.

--- src/notes/handler.clj
+++ src/notes/handler.clj
@@ -5,7 +5,7 @@
         [hiccup.page]
         [hiccup.element]
         [ring.util.response])
-  
+
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
             [monger.core :as mg]
@@ -24,75 +24,65 @@
   (mc/insert "notes" note))
 
 (defn find-notes []
-    (mc/find-maps "notes"))
+  (mc/find-maps "notes"))
 
-(defn delete-note! [id] 
+(defn delete-note! [id]
   (mc/remove-by-id "notes" (ObjectId. id)))
 
-(defn front-page [notes added?] 
-  (html 
-   (html5
+(defn front-page [notes added?]
+  (html
+    (html5
       [:head [:title "Notes"]
        (include-css "/css/bootstrap.css")
        (include-css "/css/notes.css")
        (include-js "/js/jquery-1.8.3.min.js")
        (include-js "/js/bootstrap.js")]
-    
-      [:body
-       [:div.navbar.navbar-fixed-top
-        [:div.navbar-inner
-         [:div.container
-         (link-to {:class "brand"} "#" "Notes!")]]]
-       
+
+      [:body [:div.navbar.navbar-fixed-top [:div.navbar-inner [:div.container (link-to {:class "brand"} "#" "Notes!")]]]
+
        [:header
-        [:div.container
-          [:h1 "Notes!"]
-         [:p.lead [:small " Create and read awesome notes, right here!"]]]]
-       
+        [:div.container (image {:id "note"} "/img/note.png")
+         [:div#header-txt [:h1 "Notes!"]
+         [:p.lead [:small " Create and read awesome notes, right here!"]]]]]
+
        [:div.container
-        [:div.row-fluid	
-         [:div.span4
-          [:div#add-note.affix-top {:data-spy "affix", :data-offset-top	"180"}
-      (form-to [:post "/"]
-               [:fieldset
-                 [:label "Heading"]
-                 (text-field :heading)
-                 [:label "Note"]
-                 (text-area {:rows 5} :body)
-                 [:button.btn {:type "submit"} "Create Note!"] " " (if added? [:i.icon-ok])])]]
-         
-         [:div.span8 
-          (for [note notes]
-            [:div.note
-            [:div.row-fluid 
-              [:div.span11 [:legend (:heading note)]]
-              [:div.span1
-               [:div.del (form-to [:delete "/"] (hidden-field :id (:_id note)) [:button.btn.btn-mini.btn-link {:type "submit"} [:i.icon-remove]])]]]	
-            [:div (:body note)]
-            [:p.text-info [:small (:ts note)]]])]]]]
-    
-    [:footer 
-     [:div.container 
-      [:p.muted.credit "By " (link-to "http://www.citerus.se/" "Citerus") " " [:i.icon-star-empty]
-       " Styling support by " (link-to "http://twitter.github.com/bootstrap/index.html" "Bootstrap. ") [:i.icon-star-empty]
-       " Icons by " (link-to "http://glyphicons.com/" "Glyphicons")]]])))
-    
+        [:div.row-fluid
+         [:div.span4 [:div#add-note.affix-top {:data-spy "affix", :data-offset-top "215"}
+                                                    (form-to [:post "/"]
+                                                      [:fieldset [:label "Heading"]
+                                                       (text-field :heading )
+                                                       [:label "Note"]
+                                                       (text-area {:rows 5} :body )
+                                                       [:button.btn {:type "submit"} "Create Note!"] " " (if added? [:i.icon-ok ])])]]
+
+         [:div.span8 (for [note notes]
+           [:div.note [:div.row-fluid [:div.span11 [:legend (:heading note)]]
+                                      [:div.span1 [:div.del (form-to [:delete "/"] (hidden-field :id (:_id note)) [:button.btn.btn-mini.btn-link {:type "submit"} [:i.icon-remove ]])]]]
+                      [:div (:body note)]
+                      [:p.text-info [:small (:ts note)]]])]]]]
+
+      [:footer
+       [:div.container
+        [:p.muted.credit "By " (link-to "http://www.citerus.se/" "Citerus") " " [:i.icon-star-empty ]
+                                " Styling support by " (link-to "http://twitter.github.com/bootstrap/index.html" "Bootstrap. ") [:i.icon-star-empty ]
+                                " Icons by " (link-to "http://glyphicons.com/" "Glyphicons")]]])))
+
 
 (defroutes app-routes
   (GET "/" [] (front-page (find-notes) false))
-  
-  (POST "/" [heading body] 
-        (do
-          (save-note! {:heading heading :body body :ts (DateTime.)})
-          (front-page (find-notes) true)))
-  
+
+  (POST "/" [heading body]
+    (do
+      (save-note! {:heading heading :body body :ts (DateTime.)})
+      (front-page (find-notes) true)))
+
   (DELETE "/" [id]
-         (do
-          (delete-note! id)
-          (front-page (find-notes) false)))
+    (do
+      (delete-note! id)
+      (front-page (find-notes) false)))
 
   (route/resources "/")
-  
+
   (route/not-found "Not Found"))
 
 (def app
