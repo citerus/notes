619d9cd81c1c34dfafc32448818a68518c9969d0
Håkan Jonson <hakan.jonson@gmail.com>
2013-02-21 11:38:59 +0100

    Committed missing changes.

--- project.clj
+++ project.clj
@@ -6,8 +6,10 @@
                  [com.novemberain/monger "1.4.1"]
                  [joda-time/joda-time "2.1"]
                  [hiccup "1.0.2"]
-                 [cheshire "5.0.1"]]
+                 [cheshire "5.0.1"]
+                 [org.clojure/data.json "0.2.0"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler notes.handler/app}
   :profiles
-  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
+  {:dev {:resource-paths ["resources"]
+         :dependencies [[ring-mock "0.1.3"]]}})
