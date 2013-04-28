6e0bde195b5f8d4d82ce89f48d52e77d2134a77a
Patrik Fredriksson <patrik.fredriksson@citerus.se>
2013-04-28 17:58:36 +0200

    Updated reps.

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
 (defproject notes "0.1.0-SNAPSHOT"
   :description "Simple Notes app to demo PaaS deployment"
-  :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [compojure "1.1.3"]
-                 [com.novemberain/monger "1.4.1"]
-                 [joda-time/joda-time "2.1"]
-                 [hiccup "1.0.2"]
-                 [cheshire "5.0.1"]
-                 [org.clojure/data.json "0.2.0"]]
-  :plugins [[lein-ring "0.8.2"]]
+  :url "https://github.com/citerus/notes"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.5"]
+                 [com.novemberain/monger "1.5.0"]
+                 [joda-time/joda-time "2.2"]
+                 [hiccup "1.0.3"]
+                 [cheshire "5.1.1"]
+                 [org.clojure/data.json "0.2.2"]]
+  :plugins [[lein-ring "0.8.5"]]
   :ring {:handler notes.handler/app}
   :profiles
   {:dev {:resource-paths ["resources"]
