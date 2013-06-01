cd5317cb767116951b930fc6e9d09e0881fa41e4
Patrik Fredriksson <patrik.fredriksson@citerus.se>
2013-06-02 00:11:10 +0200

    More work on PaaS connections.

--- src/notes/paas.clj
+++ src/notes/paas.clj
@@ -3,16 +3,23 @@
    [clojure.java.io :as io]))
 
 (defn is-open-shift? []
-  (System/getenv "OPEN_SHIFT"))
+  (System/getenv "OPENSHIFT_APP_UUID"))
 
 (defn is-dotcloud? []
   (-> (io/file "/home/dotcloud/environment.json") .exists))
+
+(defn is-heroku? [] false)
+
+(defn is-beanstalk [] false)
 
 ;(str "mongodb://test:test@" mongo-host ":" mongo-port "/notes")
 
 (is-dotcloud?)
 
-(defn mongo-connection-uri []
+(defn mongo-connection-uri [uid pwd]
+  (cond (is-dotcloud?) "dotcloud url"
+        (is-open-shift?) "open-shift url"
+        (is-heroku?) "heroku-url"
+        (is-beanstalk?) "mongohq-url"))
 
-
-  (str "mongodb://test:test@" mongo-host ":" mongo-port "/notes"))
+;  (str "mongodb://test:test@" mongo-host ":" mongo-port "/notes"))
