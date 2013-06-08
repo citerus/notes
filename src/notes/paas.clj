2d7b04f94ef2ed3ebed2ec83f5760dea6a9f821d
Patrik Fredriksson <patrik.fredriksson@citerus.se>
2013-06-09 00:19:50 +0200

    Added support for OpenShift. Needs testing.

--- src/notes/paas.clj
+++ src/notes/paas.clj
@@ -3,24 +3,41 @@
    [clojure.java.io :as io]
    [clojure.data.json :as json]))
 
+(defn db-name "/notes")
+
 (defn localhost-uri [uid pwd]
-   (str "mongodb://" uid ":" pwd "@localhost:27017/notes"))
+  (str "mongodb://" uid ":" pwd "@localhost:27017" db-name))
+
+;-- dotCloud
 
 (defn is-dotcloud? []
   (-> (io/file "/home/dotcloud/environment.json") .exists))
 
 (defn dotcloud-uri [uid pwd]
+  "Get dotCloud MongoDB URI. User 'uid' with password 'pwd' must exist
+  for database 'notes'"
   (let [env-file "/home/dotcloud/environment.json"
         env (with-open [f (io/reader env-file)] (json/read f))
         mongo-host (get env "DOTCLOUD_MONGO_MONGODB_HOST")
         mongo-port (get env "DOTCLOUD_MONGO_MONGODB_PORT")]
-    (str "mongodb://" uid ":" pwd "@" mongo-host ":" mongo-port "/notes")))
+    (str "mongodb://" uid ":" pwd "@" mongo-host ":" mongo-port db-name)))
+
+;-- OpenShift
 
 (defn is-open-shift? []
   (System/getenv "OPENSHIFT_APP_UUID"))
 
+(defn open-shift-uri [uid pwd]
+  "Get OpenShift MongoDB URI. 'uid' and 'pwd' are ignored, instead uses
+  OpenShift provided admin credentials for zero conf deployment."
+  (str (System/getenv "OPENSHIFT_MONGODB_DB_URL") db-name))
+
 (defn mongo-connection-uri [uid pwd]
-  (cond :else (localhost-uri uid pwd)))
+  "Try to figure out runtime environment and resolve associated MongoDB
+  connection URI. Defaults to localhost and standard port"
+  (cond (is-dotcloud?) (dotcloud-uri uid pwd)
+        (is-open-shift?) (open-shift-uri uid pwd)
+        :else (localhost-uri uid pwd)))
 
 ;;Roadmap:
 ;localhost
