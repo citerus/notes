4b2a220bd73c4778c9809c6790a005636ea85f0a
Patrik Fredriksson <patrik.fredriksson@citerus.se>
2013-06-17 23:23:07 +0200

    Adde dependencies and min-lein predicate in preparation for Heroku deployment.

--- project.clj
+++ project.clj
@@ -7,9 +7,11 @@
                  [joda-time/joda-time "2.2"]
                  [hiccup "1.0.3"]
                  [cheshire "5.1.1"]
-                 [org.clojure/data.json "0.2.2"]]
+                 [org.clojure/data.json "0.2.2"]
+                 [ring/ring-jetty-adapter "1.1.8"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler notes.handler/app}
+  :min-lein-version "2.0.0"
   :profiles
   {:dev {:resource-paths ["resources"]
          :dependencies [[ring-mock "0.1.3"]]}})
