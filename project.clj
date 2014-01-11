0b31ac506009b1f90461b90f4990c1203b4e40df
Patrik Fredriksson <patrik.fredriksson@citerus.se>
2014-01-12 00:17:33 +0100

    Re-added cheshire since it is needed by clj-time interoperability. Upgraded dependencies. Remove unused ring-mock dependency.

--- project.clj
+++ project.clj
@@ -3,15 +3,14 @@
   :url "https://github.com/citerus/notes"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]
-                 [com.novemberain/monger "1.6.0"]
+                 [com.novemberain/monger "1.7.0"]
                  [clj-time "0.6.0"]
+                 [cheshire "5.3.1"] ;required for Joda Time/Mongo intreop
                  [hiccup "1.0.4"]
-                 [org.clojure/data.json "0.2.3"]
+                 [org.clojure/data.json "0.2.4"]
                  [ring/ring-jetty-adapter "1.2.1"]]
-  :plugins [[lein-ring "0.8.8"]]
+  :plugins [[lein-ring "0.8.10"]]
   :ring {:handler notes.handler/app
-         :init notes.handler/init}
+         :init    notes.handler/init}
   :min-lein-version "2.0.0"
-  :profiles
-  {:dev {:resource-paths ["resources"]
-         :dependencies [[ring-mock "0.1.5"]]}})
+  :profiles {:dev {:resource-paths ["resources"]}})
